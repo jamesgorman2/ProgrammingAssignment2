21944ed3b055db52fee89f2cdfad2d5940485a99
James Gorman <jamesgorman2@gmail.com>
2014-11-19 14:10:43 +1100

    Add makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,27 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+##
+# Create an object containing getters and setters for
+# a matrix and its inverse. Updating the value of the matrix
+# will NULL the value of the inverse.
+##
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  setMatrix <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  getMatrix <- function() {
+    x
+  }
+  setInverse <- function(inverse) {
+    i <<- inverse
+  }
+  getInverse <- function() {
+    i
+  }
+  list(setMatrix=setMatrix, getMatrix=getMatrix, setInverse=setInverse, getInverse=getInverse)
 }
 
 
